a72cd6b5b7bbb45db2b371240f7d801b6b1cf3fa
devin <devingong@ideaculture.com>
2018-01-23 16:43:49 +0800

    ss

--- web/routes/index.js
+++ web/routes/index.js
@@ -3,7 +3,11 @@ const rootRouter = new Router();
 const apiRouter = require("./apis");
 const userRouter = require("./users");
 
+
 rootRouter.use("/api",apiRouter.routes(),apiRouter.allowedMethods());
 rootRouter.use("/user",userRouter.routes(),userRouter.allowedMethods());
+rootRouter.get("/",async(ctx) =>{
+	ctx.redirect("/statics/index.html")
+})
 
 module.exports = rootRouter;
--- web/server.js
+++ web/server.js
@@ -1,18 +1,36 @@
 const Koa = require("koa");
 const app = new Koa();
-const port = 8080
+const jwt = require("koa-jwt");
+const port = 8080;
+const secret = require("./secret.json");
 
 const bodyParser = require("koa-bodyparser");
 const staticServer = require("koa-static");
+const mount = require("koa-mount")
+
 const logger = require("koa-logger");
 const router = require("./routes/index.js");
 
 app.use(logger());
 app.use(bodyParser());
 
-app.use(router.routes());
+app.use(function(ctx, next){
+  return next().catch((err) => {
+    if (401 == err.status) {
+      ctx.status = 401;
+      ctx.body = 'Protected resource, use Authorization header to get access\n';
+    } else {
+      throw err;
+    }
+  });
+});
 
-app.use(staticServer(__dirname + "/statics"))
+app.use(jwt({secret:secret.sign}).unless({path:[/^\/api/,/^\/$/,/^\/statics\/.+/]}));
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+app.use(mount("/statics",staticServer(__dirname + "/statics")));
+
 
 app.listen(port,() =>{
 	console.log(`server listening on ${port}`);
